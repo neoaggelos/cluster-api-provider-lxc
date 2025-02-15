f5595275b7de9bc0cba58063f06e02f2a6981585
Angelos Kolaitis <neoaggelos@gmail.com>
2025-02-15 19:54:11 +0200

    add initial developer guide

--- hack/scripts/ci/setup-lxd.sh
+++ hack/scripts/ci/setup-lxd.sh
@@ -27,3 +27,17 @@ fi
 
 # Do not drop instance traffic
 sudo iptables -P FORWARD ACCEPT
+
+# Write Kubernetes secret to top-level dir
+echo "
+apiVersion: v1
+kind: Secret
+metadata:
+  name: lxc-secret
+data:
+  project: '$(echo default | base64 -w0)'
+  server: '$(echo "https://$(lxc config get core.https_address)" | base64 -w0)'
+  server-crt: '$(cat ~/snap/lxd/common/config/servercerts/local-https.crt | base64 -w0)'
+  client-crt: '$(cat ~/snap/lxd/common/config/client.crt | base64 -w0)'
+  client-key: '$(cat ~/snap/lxd/common/config/client.key | base64 -w0)'
+" | tee "${DIR}/../../lxc-secret.yaml"

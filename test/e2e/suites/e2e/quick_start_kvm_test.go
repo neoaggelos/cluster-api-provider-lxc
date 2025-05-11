9b739590b0a5e940c501b4f6b3bfbea6921afdef
Angelos Kolaitis <neoaggelos@gmail.com>
2025-05-11 12:24:00 +0300

    skip kvm test when needed, rather than rely on static tags
    Signed-off-by: Angelos Kolaitis <neoaggelos@gmail.com>

--- test/e2e/suites/e2e/quick_start_kvm_test.go
+++ test/e2e/suites/e2e/quick_start_kvm_test.go
@@ -1,23 +1,42 @@
-//go:build e2e && amd64
+//go:build e2e
 
 package e2e
 
 import (
 	"context"
 	"fmt"
+	"slices"
+	"strings"
 
 	"sigs.k8s.io/cluster-api/test/e2e"
 	"sigs.k8s.io/cluster-api/util"
 
+	"github.com/lxc/cluster-api-provider-incus/internal/incus"
 	"github.com/lxc/cluster-api-provider-incus/internal/ptr"
 	"github.com/lxc/cluster-api-provider-incus/test/e2e/shared"
 
 	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("QuickStart", func() {
 	Context("KVM", Label("PRBlocking"), Label("Flaky"), func() {
 		BeforeEach(func(ctx context.Context) {
+			client, err := incus.New(ctx, e2eCtx.Settings.LXCClientOptions)
+			Expect(err).ToNot(HaveOccurred())
+			info, _, err := client.Client.GetServer()
+			Expect(err).ToNot(HaveOccurred())
+
+			// skip if server cannot launch kvm instances
+			if !slices.Contains(strings.Split(info.Environment.Driver, " | "), "qemu") {
+				Skip(fmt.Sprintf("Server is missing driver qemu, supported drivers are: %q", info.Environment.Driver))
+			}
+
+			// skip if server is not amd64 (kvm images are only available for amd64)
+			if !slices.Contains(info.Environment.Architectures, "x86_64") {
+				Skip(fmt.Sprintf("QuickStart KVM test requires amd64, but server only supports the following architectures: %v", info.Environment.Architectures))
+			}
+
 			e2eCtx.OverrideVariables(map[string]string{
 				"WORKER_MACHINE_TYPE": "virtual-machine",
 			})

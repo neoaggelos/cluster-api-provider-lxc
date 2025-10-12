feda9e41f81e7bb904dca07048c5a3a263be146b
Angelos Kolaitis <neoaggelos@gmail.com>
2025-10-13 00:02:53 +0300

    chore: move image-builder and simplestreams under internal/exp
    Signed-off-by: Angelos Kolaitis <neoaggelos@gmail.com>

--- internal/exp/image-builder/action/action_wait.go
+++ internal/exp/image-builder/action/action_wait.go
@@ -4,12 +4,13 @@ import (
 	"context"
 	"time"
 
+	"github.com/lxc/cluster-api-provider-incus/internal/lxc"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 // Wait is an Action that blocks for the specified amount of time.
 func Wait(duration time.Duration) Action {
-	return func(ctx context.Context) error {
+	return func(ctx context.Context, _ *lxc.Client) error {
 		log.FromContext(ctx).V(1).Info("Waiting for interval", "timeout", duration)
 		select {
 		case <-ctx.Done():

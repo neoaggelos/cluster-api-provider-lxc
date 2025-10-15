1dbf521a0d24df762be13c74ae030aa022430b3b
Angelos Kolaitis <neoaggelos@gmail.com>
2025-10-15 11:29:07 +0300

    add a formal complete step for launch options to verify and validate
    Signed-off-by: Angelos Kolaitis <neoaggelos@gmail.com>

---
+++ internal/lxc/launch_options_complete.go
@@ -0,0 +1,47 @@
+package lxc
+
+import (
+	"fmt"
+	"maps"
+	"slices"
+	"strings"
+
+	"github.com/lxc/cluster-api-provider-incus/internal/utils"
+	"github.com/lxc/incus/v6/shared/api"
+)
+
+// complete launch options and verify
+func (o *LaunchOptions) complete(serverName string) error {
+	// virtual machines instances do not support unixSocket, createFiles or replacements
+	if o.instanceType == api.InstanceTypeVM {
+		if len(o.createFiles) > 0 {
+			ops := make([]string, 0, len(o.createFiles))
+			for _, f := range o.createFiles {
+				ops = append(ops, f.action())
+			}
+			return utils.TerminalError(fmt.Errorf("operations not supported for virtual-machine instances: %v", ops))
+		}
+		if len(o.replacements) > 0 {
+			return utils.TerminalError(fmt.Errorf("replacements not supported for virtual-machine instances: %v", slices.Collect(maps.Keys(o.replacements))))
+		}
+	}
+
+	// complete image configuration
+	if o.image == nil {
+		return utils.TerminalError(fmt.Errorf("cannot launch instance without image"))
+	}
+	image, err := o.image.For(serverName)
+	if err != nil {
+		return fmt.Errorf("unsupported instance image: %w", err)
+	}
+	// if OCI image is specified as `IMG[:TAG]@sha256:HASH`, replace with `IMG@sha256:HASH`
+	if image.Protocol == OCI {
+		if imageWithoutHash, hash, ok := strings.Cut(image.Alias, "@"); ok {
+			imageWithoutTag, _, _ := strings.Cut(imageWithoutHash, ":")
+			image.Alias = fmt.Sprintf("%s@%s", imageWithoutTag, hash)
+		}
+	}
+	o.image = image
+
+	return nil
+}

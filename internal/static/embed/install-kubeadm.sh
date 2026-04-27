2bd71cab48571636b09d78e58f6a5d014a71073f
Angelos Kolaitis <neoaggelos@gmail.com>
2026-04-27 03:25:35 +0300

    bump containerd, runc
    - containerd 2.2.2 -> 2.2.3
    - runc 1.3.4 -> 1.3.5
    
    Signed-off-by: Angelos Kolaitis <neoaggelos@gmail.com>

--- internal/static/embed/install-kubeadm.sh
+++ internal/static/embed/install-kubeadm.sh
@@ -8,8 +8,8 @@ set -xeu
 KUBERNETES_VERSION="${KUBERNETES_VERSION:-$1}"            # https://dl.k8s.io/release/stable.txt or https://dl.k8s.io/release/stable-1.32.txt
 CNI_PLUGINS_VERSION="${CNI_PLUGINS_VERSION:-v1.9.1}"      # https://github.com/containernetworking/plugins
 CRICTL_VERSION="${CRICTL_VERSION:-v1.35.0}"               # https://github.com/kubernetes-sigs/cri-tools
-CONTAINERD_VERSION="${CONTAINERD_VERSION:-v2.2.2}"        # https://github.com/containerd/containerd
-RUNC_VERSION="${RUNC_VERSION:-v1.3.4}"                    # https://github.com/opencontainers/runc, must match https://raw.githubusercontent.com/containerd/containerd/v2.2.2/script/setup/runc-version
+CONTAINERD_VERSION="${CONTAINERD_VERSION:-v2.2.3}"        # https://github.com/containerd/containerd
+RUNC_VERSION="${RUNC_VERSION:-v1.3.5}"                    # https://github.com/opencontainers/runc, must match https://raw.githubusercontent.com/containerd/containerd/v2.2.3/script/setup/runc-version
 
 KUBELET_SERVICE='
 # Sourced from: https://raw.githubusercontent.com/kubernetes/release/v0.21.0/cmd/krel/templates/latest/kubelet/kubelet.service
